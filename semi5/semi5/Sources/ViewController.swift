1e611c49a0ba99c15b9427bc87da2e6345ae43ab
juyeonblue <jyp0211@ajou.ac.kr>
2019-11-19 15:04:39 +0900

    semi2_assignment

---
+++ semi5/semi5/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  semi5
+//
+//  Created by 박주연 on 19/11/2019.
+//  Copyright © 2019 박주연. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
